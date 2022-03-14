3a7d369d50872a5bc102c7b5deb5413d3e7919c3
Marcelle Mendes <marcelleemendess@mymac.local>
2022-03-14 17:45:15 -0600

    junior test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState} from 'react'
+import Pagination from './components/Pagination';
 
 function App() {
+
+  const [ itens, setItens] = useEffect([]);
+  const [itensPerPage, setItensPerPage] = useState(10)
+  const [currentPage, setCurrentPage] = useState(0)
+
+  const pages = Math.ceil(itens.length / itensPerPage)
+  const startIndex = currentPage * itensPerPage;
+  const endIndex = startIndex + itensPerPage;
+  const currentItens = itens.slice(startIndex, endIndex)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(data => data)
+      console.log(result)
+
+      setItens(result)
+    }
+    fetchData() 
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {currentItens?.map(item => {
+        return <div className="item"><span>{item.id}</span><span>{item.title}</span><spa>{item.completed}</spa></div>
+      })}
+      <Pagination pages={pages} setCurrentPage={setCurrentPage}/>
     </div>
   );
 }
---
+++ src/components/Pagination.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Pagination = ({pages, setCurrentPage}) => {
+    return ( 
+        <div>
+            {Array.from(Array(pages),(item, index) => {
+            return <button value={index} onClick={(e) => setCurrentPage(Number(e.target.value))}>{index + 1}</button>
+            })}
+         </div>
+         
+    )
+
+}
+
+export default Pagination;
